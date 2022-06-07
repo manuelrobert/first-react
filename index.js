a5360d2e6c50cb5b5dddc9d1a96c8f148934d796
Manuel Robert <manuelrobz@outlook.com>
2022-06-07 21:46:32 +0530

    understanding JSX

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-function NavBar(){
-    return (
+// function NavBar(){
+//     return (
+//         <ul>
+//             <li><a href="#">Home</a></li>
+//             <li><a href="#">Link</a></li>
+//             <li><a href="#">Something else here</a></li>
+//             <li><a href="#">Disabled</a></li>
+//         </ul>
+//     )
+// }
+// function MainContent(){
+//     return (
+//         <h1>I'm learning React</h1>
+//     )
+// }
+// ReactDOM.render(
+//     <div>
+//         <NavBar/>
+//         <MainContent/>
+//     </div>, 
+//     document.getElementById("root")
+// )  
+
+const navbar = (
+    <nav>
+        <h1>
+            Menu A
+        </h1>
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li><a href="#">Disabled</a></li>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
         </ul>
-    )
-}
-function MainContent(){
-    return (
-        <h1>I'm learning React</h1>
-    )
-}
-ReactDOM.render(
-    <div>
-        <NavBar/>
-        <MainContent/>
-    </div>, 
-    document.getElementById("root")
-)  
+    </nav>
+)
+console.log(navbar)
+
+ReactDOM.render(navbar, document.getElementById("root"))
